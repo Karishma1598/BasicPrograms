dfcc8f669efc4933f79558e432c49e6887c3e003
Karishma Gulunjkar <karishmagulu@gmail.com>
2025-04-14 13:17:43 +0530

    modified privious file

--- Basics/SwapTwoNumbers.java
+++ Basics/SwapTwoNumbers.java
@@ -18,6 +18,21 @@ public class SwapTwoNumbers {
         System.out.println("Numbers after swapping are no1="+no1+
                 " and no2="+no2);
 
+        //we can swap without 3rd variable
+        System.out.println();
+        System.out.println();
+
+        int a=10;
+        int b=20;
+        System.out.println("swap without using 3rd variable");
+        System.out.println("before swapping a="+a+" and b="+b);
+
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("After swapping a="+a+" and b="+b);
+
+
 
     }
 
