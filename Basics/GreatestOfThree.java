cb8b68cfa42cbc4c8ead345ad45e2556f4602b36
Karishma Gulunjkar <karishmagulu@gmail.com>
2025-04-22 16:43:36 +0530

    add some basic files of basics

---
+++ Basics/GreatestOfThree.java
@@ -0,0 +1,24 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class GreatestOfThree {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the 1st number");
+        int num1=sc.nextInt();
+
+        System.out.println("Enter the 1st number");
+        int num2=sc.nextInt();
+
+        System.out.println("Enter the 1st number");
+        int num3=sc.nextInt();
+
+        if(num1>=num2 && num1>=num3)
+            System.out.println("number 1 i.e "+num1+" is greatest");
+        else if (num2>=num3)
+            System.out.println("number 2 i.e "+num2+" is greatest");
+        else
+            System.out.println("number 3 i.e "+num3+" is greatest");
+    }
+}
