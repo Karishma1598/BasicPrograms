91d7792a968601472a521f5e7e37083226324c0b
Karishma Gulunjkar <karishmagulu@gmail.com>
2025-04-22 16:09:24 +0530

    add file leap year of basics

---
+++ Basics/LeapYear.java
@@ -0,0 +1,19 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void isLeapYear(int year){
+        if(year%400==0 ||(year%4==0 && year%100!=0)){
+            System.out.println("This is leap year");
+        }else {
+            System.out.println("It is not a leap year");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the year");
+        int year=sc.nextInt();
+        isLeapYear(year);
+    }
+}
