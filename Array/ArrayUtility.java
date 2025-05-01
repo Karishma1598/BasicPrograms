48503868ca306b6ca4c176200074357ece9a4352
Karishma Gulunjkar <karishmagulu@gmail.com>
2025-05-01 16:40:31 +0530

    modify some file

--- Array/ArrayUtility.java
+++ Array/ArrayUtility.java
@@ -14,18 +14,44 @@ public class ArrayUtility {
         }
         return arr;
     }
+    public static int[][] input2DArray(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter no of rows");
+        int rows=sc.nextInt();
+        System.out.println("Enter no of columns");
+        int col=sc.nextInt();
+        int[][] arr=new int[rows][col];
+        for (int i=0;i<arr.length;i++){
+            for (int j=0;j<col;j++){
+                System.out.print("Please enter ele  rows "+(i+1)+", Column "+
+                        (j+1)+" :");
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        return arr;
+    }
     public static void displayArray(int[] arr){
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
+        for(int ele:arr){
+            System.out.print(ele+" ");
+        }
+    }
+    public static void display2Darray(int[][] arr){
+        for (int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
         }
     }
 
-    /*
+
+
     public static void main(String[] args) {
 
-        ArrayUtility.displayArray(ArrayUtility.inputArray());
+       // ArrayUtility.displayArray(ArrayUtility.inputArray());
+        //ArrayUtility.display2Darray(ArrayUtility.input2DArray());
 
     }
 
-     */
+
 }
