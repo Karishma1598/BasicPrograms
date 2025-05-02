e614fbdfe04e5f8cae4192b57211027e70a30d1a
Karishma Gulunjkar <karishmagulu@gmail.com>
2025-05-02 15:37:08 +0530

    added some basic files

---
+++ Basics/FindGrades.java
@@ -0,0 +1,23 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class FindGrades {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your percentage");
+        float per=sc.nextFloat();
+
+        if(per>=90){
+            System.out.println("You have got grade A");
+        }else if(per>=75){
+            System.out.println("You have got grade B");
+        }else if(per>=60){
+            System.out.println("You have got grade C");
+        } else if (per>=35) {
+            System.out.println("You have got grade D");
+        }else {
+            System.out.println("You have failed the exam");
+        }
+    }
+}
